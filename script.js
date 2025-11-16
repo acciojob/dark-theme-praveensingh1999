f4e273ec367ab805cec436dc95975099e4c5b257
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-11-16 14:54:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const swap = document.querySelector("#swap");
+const app = document.querySelector("#app");
 
 function swapTheme() {
     if (swap.classList.contains("button_day")) {
+
+        // Switch to night mode
         swap.classList.remove("button_day");
         swap.classList.add("button_night");
+
+        app.classList.remove("day");
+        app.classList.add("night");
+
     } else {
+
+        // Switch to day mode
         swap.classList.remove("button_night");
         swap.classList.add("button_day");
+
+        app.classList.remove("night");
+        app.classList.add("day");
     }
 }
